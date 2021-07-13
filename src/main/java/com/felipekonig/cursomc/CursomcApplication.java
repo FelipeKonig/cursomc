72981957a5e904db68d9b75cb333bc1f9fa286fc
FelipeKonig <49540283+FelipeKonig@users.noreply.github.com>
2021-07-13 15:13:02 -0300

    Criando operacao de instanciacao

--- src/main/java/com/felipekonig/cursomc/CursomcApplication.java
+++ src/main/java/com/felipekonig/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.felipekonig.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.felipekonig.cursomc.domain.Categoria;
+import com.felipekonig.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repoCategoria;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		repoCategoria.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
