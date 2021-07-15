8291d09c435d201e7ea8bef7677a7c0dbc7a3af7
FelipeKonig <49540283+FelipeKonig@users.noreply.github.com>
2021-07-15 15:50:33 -0300

    Deletando Categoria com Delete

--- src/main/java/com/felipekonig/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/felipekonig/cursomc/resources/CategoriaResource.java
@@ -40,9 +40,18 @@ public class CategoriaResource {
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+		
 		obj.setId(id);
 		obj = service.update(obj);
 
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+	
+		service.delete(id);
+		
 		return ResponseEntity.noContent().build();
 	}
 
--- src/main/java/com/felipekonig/cursomc/services/CategoriaService.java
+++ src/main/java/com/felipekonig/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.felipekonig.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.felipekonig.cursomc.domain.Categoria;
 import com.felipekonig.cursomc.repositories.CategoriaRepository;
+import com.felipekonig.cursomc.services.exceptions.DataIntegrityException;
 import com.felipekonig.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,9 +20,9 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		Optional<Categoria> cat = repo.findById(id);
 		return cat.orElseThrow(() -> new ObjectNotFoundException(
-				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
@@ -29,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+
 	}
 }
