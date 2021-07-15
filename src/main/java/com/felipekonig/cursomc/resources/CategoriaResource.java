63b937f32e781355555a983ec6600f57957747b3
FelipeKonig <49540283+FelipeKonig@users.noreply.github.com>
2021-07-15 16:13:50 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/felipekonig/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/felipekonig/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.felipekonig.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.felipekonig.cursomc.DTO.CategoriaDTO;
 import com.felipekonig.cursomc.domain.Categoria;
 import com.felipekonig.cursomc.services.CategoriaService;
 
@@ -37,23 +39,31 @@ public class CategoriaResource {
 
 		return ResponseEntity.created(uri).build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
-		
+
 		obj.setId(id);
 		obj = service.update(obj);
 
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
-	
+
 		service.delete(id);
-		
+
 		return ResponseEntity.noContent().build();
 	}
 
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).toList();
+
+		return ResponseEntity.ok().body(listDTO);
+	}
 
 }
