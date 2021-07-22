5620bb40af23715645a64c5a65aee9557f05f81b
FelipeKonig <49540283+FelipeKonig@users.noreply.github.com>
2021-07-22 18:22:42 -0300

    Email HTML

--- src/main/java/com/felipekonig/cursomc/services/PedidoService.java
+++ src/main/java/com/felipekonig/cursomc/services/PedidoService.java
@@ -68,7 +68,7 @@ public class PedidoService {
 		}
 		repoItemPedido.saveAll(obj.getItens());
 
-		emailService.sendOrderConfirmationEmail(obj);
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 
 		return obj;
 	}
--- src/main/java/com/felipekonig/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/felipekonig/cursomc/services/SmtpEmailService.java
@@ -1,24 +1,36 @@
 package com.felipekonig.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
-public class SmtpEmailService extends AbstractEmailService{
-	
+public class SmtpEmailService extends AbstractEmailService {
+
 	@Autowired
 	private MailSender mailSender;
 	
-	private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(MockEmailService.class);
+	@Autowired
+	private JavaMailSender javaMailSender;
 
+	private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(MockEmailService.class);
 
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Enviando de email...");
 		mailSender.send(msg);
 		LOG.info("Email enviado");
-		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando de email...");
+		javaMailSender.send(msg);
+		LOG.info("Email enviado");
+
 	}
 
 }
