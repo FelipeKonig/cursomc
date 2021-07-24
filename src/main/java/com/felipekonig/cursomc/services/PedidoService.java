236a1b47207f6f0b366385212ade7d42b82752d3
FelipeKonig <49540283+FelipeKonig@users.noreply.github.com>
2021-07-24 15:39:52 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/felipekonig/cursomc/services/PedidoService.java
+++ src/main/java/com/felipekonig/cursomc/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.felipekonig.cursomc.domain.Cliente;
 import com.felipekonig.cursomc.domain.ItemPedido;
 import com.felipekonig.cursomc.domain.PagamentoComBoleto;
 import com.felipekonig.cursomc.domain.Pedido;
@@ -13,6 +17,8 @@ import com.felipekonig.cursomc.domain.enums.EstadoPagamento;
 import com.felipekonig.cursomc.repositories.ItemPedidoRepository;
 import com.felipekonig.cursomc.repositories.PagamentoRepository;
 import com.felipekonig.cursomc.repositories.PedidoRepository;
+import com.felipekonig.cursomc.security.UserSS;
+import com.felipekonig.cursomc.services.exceptions.AuthorizationException;
 import com.felipekonig.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -35,7 +41,7 @@ public class PedidoService {
 
 	@Autowired
 	private BoletoService boletoService;
-	
+
 	@Autowired
 	private EmailService emailService;
 
@@ -71,5 +77,19 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 
 		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+
+		UserSS user = UserService.authenticated();
+
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		Cliente cliente = clienteService.find(user.getId());
+
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
